b645b28dce3ed7bb384ec77c92994444cd15c1c7
Duncan Gatuthu <dgatuthu@gmail.com>
2023-10-10 17:06:04 +0300

    improvement: make full blog link button clickable

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -13,17 +13,10 @@ const Blogs = () => {
         <h1 className="text-xl sm:text-2xl lg:text-3xl 2xl:text-4xl font-semibold">Blog Posts</h1>
 
         {/* Other Blog links */}
-        <div className="mt-4">
-          <ul className="flex justify-between gap-6">
-            {blogLinks.map(({label, href}) => (
-              <li className="flex-1 bg-secondaryLight dark:bg-secondaryDark text-lg font-semibold rounded-md text-center py-2 px-8" key={label}>
-                <Link href={href}
-                key={label}>
-                  <span>{label}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <div className="mt-4 flex justify-between gap-6">
+          {blogLinks.map(({label, href}) => (
+            <Link href={href} target="_blank" className="flex-1 bg-secondaryLight dark:bg-secondaryDark text-lg font-semibold rounded-md text-center py-2 px-8">{label}</Link>
+          ))}
         </div>
         {/* Search area */}
         {/* Blog tiles */}
