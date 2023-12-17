8370133e636446daec9d05de6cc7c5a1e6139e49
Duncan Gatuthu <60921359+hellodun@users.noreply.github.com>
2023-12-17 22:52:00 +0300

    Update page.tsx, fix more special character errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
         {/* Paragraph */}
         <p className="text-lg sm:text-xl xl:text-2xl 2xl:text-4xl font-medium space-y-3 2xl:space-y-6">
           <span className="block">
-            <span className="font-bold">Hi, I&apos;m Duncan,</span> a System
+            <span className="font-bold">Hi, I am Duncan,</span> a System
             Admin and Azure cloud engineer. I also do software development on
             the side.
           </span>
@@ -19,11 +19,11 @@ export default function About() {
             I am currently working on some projects using Go and also upskilling on Kubernetes, using the CNCF's Book Club community.
           </span>
           <span className="block">
-            I have a blog where I&apos;ll be documenting my learning especially
+            I have a blog where I will be documenting my learning especially
             in cloud and also software development, feel free to check it out.
           </span>
           <span className="block">
-            I learn best by doing projects and I&apos;ll be documenting some
+            I learn best by doing projects and I will be documenting some
             projects and demos here.
           </span>
         </p>
